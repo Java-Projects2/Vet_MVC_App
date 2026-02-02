db6960038dd3316385e99b5a60610ded58627dd7
noureldin <noureldin2662002@gmail.com>
2026-02-02 23:06:54 +0200

    Home and layout

--- src/main/java/com/example/vet_mvc_app/HomeController.java
+++ src/main/java/com/example/vet_mvc_app/HomeController.java
@@ -1,14 +1,18 @@
 package com.example.vet_mvc_app;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String simplePage() {
-        return "/HomePage";
+    public String simplePage(Model model) {
+        model.addAttribute("pageTitle", "Home Page");
+        model.addAttribute("view", "HomePage");
+        model.addAttribute("page", "home");
+        return "/Layout";
     }
 
 }
